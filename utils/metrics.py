efcfb7279d03044f3a09f266fd18907423022a14
LoneX <elouanes.khelifi@uqtr.ca>
2022-09-27 11:04:40 -0400

    1 - Diou

--- utils/metrics.py
+++ utils/metrics.py
@@ -249,7 +249,7 @@ def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, FIoU = F
                 with torch.no_grad():
                     alpha = v / (v - iou + (1 + eps))
                 if FIoU:
-                    DIoU = iou - rho2 / c2
+                    DIoU = 1 - rho2 / c2
                     fiou = lxFuzz.compute_FIoU(DIoU, v, iou)#fuzzy IoU score using DIoU, consistensy of aspect ratio, and iou
                     return iou - fiou
                 CIoU = iou - (rho2 / c2 + v * alpha)
